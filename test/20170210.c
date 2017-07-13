e9fc8edde7e06792266c22285ea0b6fe1d695694
jackonly06 <jackonly06@hotmail.com>
2017-07-13 17:15:39 +0800

    edit 1 file

--- test/20170210.c
+++ test/20170210.c
@@ -1,45 +1,24 @@
-nclude <stdio.h>
-
-
+#include <stdio.h>
 
 void test(void)
-
 {
-
-//  char NDX_aTimeSeparator[2] = {':','\0'}; 
-
-//  char                a_wk_str[8];
-
-//  int                 n_r = 0 ,n_q = 0;
-
-    char    buff[50];
-
-    unsigned char   r, g, b;
-
-
-
-    r = 0x00ff;  
-
-    g = 0x0000ff;  
-
-    b = 0x000000ff;  
-
-  //c = sprintf( &a_wk_str[0], "%02d%s%02d", n_r, &NDX_aTimeSeparator[0],n_q); 
-
-    _snprintf_s(buff, sizeof(buff) - 1, sizeof(buff) - 1, "%4.2f %4.2f %4.2f RGB\n", (float)r / 255.0, (float)g / 255.0, (float)b / 255.0);
-
+    //  char NDX_aTimeSeparator[2] = {':','\0'};
+    //      //  char                a_wk_str[8];
+    //          //  int                 n_r = 0 ,n_q = 0;
+    char    buff[32];
+    unsigned char   r;
+    unsigned char   addr[4] = {0};
+    r = 0x00ff;
+    //c = sprintf( &a_wk_str[0], "%02d%s%02d", n_r, &NDX_aTimeSeparator[0],n_q);
+    sprintf(buff, "%03d.%03d.%03d.%03d", addr[3], addr[2], addr[1], addr[0]);
     printf("%s\n", buff);
-
 }
-
-
 
 int main(void)
-
 {
-
+    //    int a=1,b=2;
+    //    Myprintf("a=%d,b=%d",a,b);
     test();
-
-    return 0; 
-
+    return 0;
 }
+
