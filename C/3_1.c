ecb8b9bf84d9ab99b9a1a6560eb80a42fb01013e
jackonly06 <jackonly06@hotmail.com>
2020-04-22 23:23:36 +0800

    create link list

--- C/3_1.c
+++ C/3_1.c
@@ -6,6 +6,8 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAXSIZE 20
 #define ERROR   0
@@ -20,6 +22,13 @@ typedef struct{
     int iLength;
 } SQLIST;
 
+typedef struct Node{
+    ElemType data;
+    struct Node *next;
+} Node;
+
+typedef struct Node *LinkList;
+
 Status GetElem(SQLIST L, int i, ElemType *e)
 {
     if( L.iLength == 0 || i < 0 || i > L.iLength ){
@@ -29,8 +38,30 @@ Status GetElem(SQLIST L, int i, ElemType *e)
     return TURE;
 }
 
+void CreateListHead(LinkList *L, int n)
+{
+    LinkList p;
+    int i;
+
+    srand(time(0)); /* create random data */
+    *L = (LinkList)malloc(sizeof(Node));
+    (*L)->next = NULL; /* set head node */
+    for( i = 0; i < n; i++ ){
+        p = (LinkList)malloc(sizeof(Node));
+        p->data = rand() % 100 + 1;
+        p->next = (*L)->next;
+        (*L)->next = p; /* insert to head */
+    }
+}
+
 int main(int argc, char **argv)
 {
+    LinkList P;
+    CreateListHead(&P,5);
+    while(P){
+        printf("%d\n", P->data);
+        P = P->next;
+    }
     return 0;
 }
 
